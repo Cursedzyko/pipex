2b6cfb3297592740f5aa9514d315c26db05767d1
Zafarbek <zyunusov@student.42wolfsburg.de>
2022-10-22 13:38:12 +0300

    need to create func to check full path and copy to bonus and check in bonus infile, outfile, here_doc error

--- srcs/child_utils.c
+++ srcs/child_utils.c
@@ -6,7 +6,7 @@
 /*   By: zyunusov <zyunusov@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/16 11:50:49 by zyunusov          #+#    #+#             */
-/*   Updated: 2022/10/20 13:48:34 by zyunusov         ###   ########.fr       */
+/*   Updated: 2022/10/22 13:33:56 by zyunusov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,15 @@ void	child_in(t_data data, char **argv, char **envp)
 	close_pipes(&data);
 	close(data.infile);
 	parse_args(&data, argv[2]);
-	data.cmd = get_cmd(data.path_cmd, data.args_cmd[0]);
+	if (ft_strchr(data.args_cmd[0], '/') != 0)
+	{
+		if (access(data.args_cmd[0], F_OK) == 0)
+			data.cmd = data.args_cmd[0];
+		else
+			ft_error_f(data.args_cmd[0], "No such file or directory", CMD_NOT_FOUND);
+	}
+	if (ft_strchr(data.args_cmd[0], '/') == 0)
+		data.cmd = get_cmd(data.path_cmd, data.args_cmd[0]);
 	if (!data.cmd)
 	{
 		ft_error_f("command not found", data.args_cmd[0], CMD_NOT_FOUND);
@@ -51,7 +59,15 @@ void	child_out(t_data data, char **argv, char **envp)
 	close_pipes(&data);
 	close(data.outfile);
 	parse_args(&data, argv[3]);
-	data.cmd = get_cmd(data.path_cmd, data.args_cmd[0]);
+	if (ft_strchr(data.args_cmd[0], '/') != 0)
+	{
+		if (access(data.args_cmd[0], F_OK) == 0)
+			data.cmd = data.args_cmd[0];
+		else
+			ft_error_f(data.args_cmd[0], "No such file or directory", CMD_NOT_FOUND);
+	}
+	if (ft_strchr(data.args_cmd[0], '/') == 0)
+		data.cmd = get_cmd(data.path_cmd, data.args_cmd[0]);
 	if (!data.cmd)
 	{
 		ft_error_f("command not found", data.args_cmd[0], CMD_NOT_FOUND);
